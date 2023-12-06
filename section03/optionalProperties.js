7111e75fc6e642e37ba352ad58f117577287b987
Tutku Ince <tutku.ince@outlook.com>
2023-12-06 22:53:38 +0300

    read only modifier

---
+++ section03/optionalProperties.js
@@ -0,0 +1,8 @@
+var myPoint = { x: 1, y: 2 }; // z is optional not mandatory.
+var user = {
+    id: 123123,
+    username: "user1"
+};
+console.log(user);
+console.log(user.id);
+// user.id = 123123; Cannot assign to id because it is a read-only property.
--- section03/optionalProperties.ts
+++ section03/optionalProperties.ts
@@ -5,3 +5,16 @@ type Point = {
 }
 
 const myPoint: Point = {x: 1, y: 2}; // z is optional not mandatory.
+
+type User = {
+    readonly id: number;
+    username: string
+};
+
+const user: User = {
+    id: 123123,
+    username: "user1"
+}
+console.log(user);
+console.log(user.id);
+// user.id = 123123; Cannot assign to id because it is a read-only property.
