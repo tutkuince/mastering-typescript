96dc00e44a12edcb7a41af44caddcd83ed44a369
Tutku Ince <tutku.ince@outlook.com>
2023-12-06 23:02:44 +0300

    intersection types

--- section03/optionalProperties.ts
+++ section03/optionalProperties.ts
@@ -18,3 +18,31 @@ const user: User = {
 console.log(user);
 console.log(user.id);
 // user.id = 123123; Cannot assign to id because it is a read-only property.
+
+type Circle = {
+    radius: number;
+};
+
+type Colorful = {
+    color: string;
+}
+
+type ColorfulCircle = Circle & Colorful; // Intersection Types
+const happyFace: ColorfulCircle = {radius: 4, color: "yellow"};
+
+
+type Cat = {
+    numLives: number;
+}
+
+type Dog = {
+    breed: string;
+}
+
+type CatDog = Cat & Dog & { age: number };
+
+const catDog: CatDog = {
+    numLives: 7,
+    age: 5,
+    breed: "Terrier"
+};
