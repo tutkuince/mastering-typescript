f9f3bf41018e679548f421c5dbfdbb4b14ac5b96
Tutku Ince <tutku.ince@outlook.com>
2023-12-08 18:11:41 +0300

    more array syntax

--- section04/arrays.ts
+++ section04/arrays.ts
@@ -4,3 +4,16 @@ activeUsers.push("Tutku");
 const ageList: number[] = [45, 56, 13.5];
 ageList[0] = 123;
 console.log(ageList);
+
+// An alternate syntax
+let names: Array<string> = ["hello", "world"];
+let ages: Array<number> = [23, 31, 18, 29];
+
+type Point = {
+    x: number;
+    y: number;
+}
+
+const coords: Array<Point> = [];
+coords.push({x: 23, y:18});
+coords.push({x: 12, y:38});
