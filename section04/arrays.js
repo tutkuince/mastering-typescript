209dcb388bd0485a1b2d56f5a7f7d5e425c7104d
Tutku Ince <tutku.ince@outlook.com>
2023-12-08 18:08:02 +0300

    Working with array types

---
+++ section04/arrays.js
@@ -0,0 +1,5 @@
+var activeUsers = ["Tutku"];
+activeUsers.push("Tutku");
+var ageList = [45, 56, 13.5];
+ageList[0] = 123;
+console.log(ageList);
---
+++ section04/arrays.ts
@@ -0,0 +1,6 @@
+const activeUsers: string[] = ["Tutku"];
+activeUsers.push("Tutku");
+
+const ageList: number[] = [45, 56, 13.5];
+ageList[0] = 123;
+console.log(ageList);
