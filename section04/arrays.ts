82d0617bb430cb94dbcb2511d9b65266d6d52380
Tutku Ince <tutku.ince@outlook.com>
2023-12-08 18:14:34 +0300

    Multidimensional Arrays

--- section04/arrays.ts
+++ section04/arrays.ts
@@ -17,3 +17,10 @@ type Point = {
 const coords: Array<Point> = [];
 coords.push({x: 23, y:18});
 coords.push({x: 12, y:38});
+
+// Multidimensional Arrays
+const board: string[][] = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"]
+];
