3c38bf0c74d20c6978fdbc14aca161a5c25f0001
Tutku Ince <tutku.ince@outlook.com>
2023-12-09 20:37:29 +0300

    type narrowing

--- section05/unionTypes.ts
+++ section05/unionTypes.ts
@@ -14,3 +14,10 @@ type Loc = {
 
 let coordinates: Point | Loc =  {x: 1, y:35};
 coordinates = {lat: 321.123, long: 23.123};
+
+function printAge(age: number | string): void {
+    console.log(`You are ${age} years old`);
+}
+
+printAge(34);
+printAge("25");
