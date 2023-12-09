d343ad5a02605e41e57bed37acb5303750216f77
Tutku Ince <tutku.ince@outlook.com>
2023-12-09 20:36:23 +0300

    Union types

--- section05/unionTypes.ts
+++ section05/unionTypes.ts
@@ -1,3 +1,16 @@
 let age: number | string | boolean = 21;
 age = "23";
 age = true;
+
+type Point = {
+    x: number;
+    y: number
+};
+
+type Loc = {
+    lat: number;
+    long: number;
+}
+
+let coordinates: Point | Loc =  {x: 1, y:35};
+coordinates = {lat: 321.123, long: 23.123};
