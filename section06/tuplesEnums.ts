743e273b97c43f0a7797be87d90ff21556e4adc0
Tutku Ince <tutku.ince@outlook.com>
2023-12-11 22:12:48 +0300

    more on tuples

--- section06/tuplesEnums.ts
+++ section06/tuplesEnums.ts
@@ -1,1 +1,6 @@
 const color: [number, number, number] = [123, 324, 345];
+
+type HTTPResponse = [number, string];
+const goodRes: HTTPResponse = [200, "OK"];
+
+const respnses: HTTPResponse[] = [[404, "Not Found"], [200, "OK"]];
