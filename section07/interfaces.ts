b14a77fd053903aec9dfc967f9f26ee7da4ac671
Tutku Ince <tutku.ince@outlook.com>
2023-12-13 22:00:16 +0300

    readonly and optional interface properties

--- section07/interfaces.ts
+++ section07/interfaces.ts
@@ -9,3 +9,13 @@ const sayHappyBirthday = (person: Person): string => {
 
 const person: Person = {name: "Tutku", age: 33};
 console.log(sayHappyBirthday(person));
+
+interface Point {
+    readonly id: number;
+    x: number;
+    y: number;
+    z?: number;
+}
+
+const pt: Point = {id: 123321, x: 123, y: 321};
+// pt.id = 123; Cannot assign to id because it is a read-only property.
