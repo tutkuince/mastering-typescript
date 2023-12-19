a41f69ff4afe8f31639751b976a2e3c0101865b4
Tutku Ince <tutku.ince@outlook.com>
2023-12-19 21:32:19 +0300

    private modifier

--- section09/dist/index.js
+++ section09/dist/index.js
@@ -5,7 +5,11 @@ class Player {
         this.first = first;
         this.last = last;
     }
+    privateMethod() {
+        console.log("This is a private method, only accessible in Player class.");
+    }
 }
 const barney = new Player("Barney", "Stinson");
 // barney.last = "tot"; => Attempt to assign to const or readonly variable
+// barney.first = "Tot"; => Property first is private and only accessible within class Player
 console.log(barney.score);
--- section09/src/index.ts
+++ section09/src/index.ts
@@ -1,13 +1,19 @@
 class Player {
-    public readonly first: string; // public by default
-    public readonly last: string;
+    private readonly first: string; // public by default
+    private readonly last: string;  // we can use "readonly #first". It is the same thing.
     public score: number = 0;
     constructor(first: string, last: string) {
         this.first = first;
         this.last = last;
+
+    }
+
+    private privateMethod(): void {
+        console.log("This is a private method, only accessible in Player class.");
     }
 }
 
 const barney = new Player("Barney", "Stinson");
 // barney.last = "tot"; => Attempt to assign to const or readonly variable
+// barney.first = "Tot"; => Property first is private and only accessible within class Player
 console.log(barney.score);
