bfedcd27b36748910ebb036fcc378b3fe3dc65fc
Tutku Ince <tutku.ince@outlook.com>
2023-12-19 21:39:56 +0300

    getters and setters

--- section09/dist/index.js
+++ section09/dist/index.js
@@ -14,18 +14,33 @@
 //     }
 // }
 class Player {
-    constructor(first, last) {
+    // private readonly first: string; // public by default
+    // private readonly last: string;  // we can use "readonly #first". It is the same thing.
+    // public score: number = 0;
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
-        // private readonly first: string; // public by default
-        // private readonly last: string;  // we can use "readonly #first". It is the same thing.
-        this.score = 0;
+        this._score = _score;
     } // shortcut
     privateMethod() {
         console.log("This is a private method, only accessible in Player class.");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        this._score = newScore;
+    }
 }
-const barney = new Player("Barney", "Stinson");
+const barney = new Player("Barney", "Stinson", 100);
 // barney.last = "tot"; => Attempt to assign to const or readonly variable
 // barney.first = "Tot"; => Property first is private and only accessible within class Player
+barney.score = 150;
 console.log(barney.score);
+console.log(barney.fullName);
--- section09/src/index.ts
+++ section09/src/index.ts
@@ -16,15 +16,34 @@
 class Player {
     // private readonly first: string; // public by default
     // private readonly last: string;  // we can use "readonly #first". It is the same thing.
-    public score: number = 0;
-    constructor(public first: string, public last: string) {} // shortcut
+    // public score: number = 0;
+    constructor(public first: string, public last: string, private _score: number) {} // shortcut
 
     private privateMethod(): void {
         console.log("This is a private method, only accessible in Player class.");
     }
+
+    get fullName(): string {
+        return `${this.first} ${this.last}`;
+    }
+
+    get score(): number {
+        return this._score;
+    }
+
+    set score(newScore: number) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        this._score = newScore;
+    }
 }
 
-const barney = new Player("Barney", "Stinson");
+const barney = new Player("Barney", "Stinson", 100);
 // barney.last = "tot"; => Attempt to assign to const or readonly variable
 // barney.first = "Tot"; => Property first is private and only accessible within class Player
+barney.score = 150;
 console.log(barney.score);
+console.log(barney.fullName);
+
+
